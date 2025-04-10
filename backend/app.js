0b49b940781153cb28fe535136841eea08f66ad5
huynhduc2412 <huynhducmapu2002@gmail.com>
2025-04-10 11:13:45 +0700

    add

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ const connectDB = require('./config/db');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 
 var app = express();
@@ -27,8 +26,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api', loginRouter);
+app.use('/register', require('./routes/register'));
+app.use('/verify', require('./routes/verify'));
+app.use('/api/users' , require('./routes/user'))
+app.use('/cloudinary', require('./routes/upload'))
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
